31553a9516b942dbd98131104353eb560a08064f
an1mehhh <an1mehhh@yandex.ru>
2023-10-30 01:29:37 +0300

    Command to clear the database

---
+++ catalog/management/commands/clear_db.py
@@ -0,0 +1,9 @@
+from django.core.management import BaseCommand
+from catalog.models import Product, Category
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        # Category.objects.all().delete()
+        Product.objects.all().delete()
