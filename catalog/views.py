3838d34cdee382c275fad7d82b9d9ebc96dc7b01
an1mehhh <an1mehhh@yandex.ru>
2023-11-18 15:50:54 +0300

    update views.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
+from catalog.models import Product, Category
 
 
 def home(request):
-    return render(request, 'catalog/home.html')
+    context = {
+        'object_list': Category.objects.all(),
+        'title': 'Skystore',
+        'description': 'Skystore - это отличный вариант хранения ваших плагинов и примеров кода, который вы бы хотели продать',
+    }
+    return render(request, 'catalog/home.html', context)
 
 
 def contacts(request):
     return render(request, 'catalog/contacts.html')
+
+
+def products(request, pk):
+    context = {
+        'object_list': Product.objects.filter(category_id=pk)
+    }
+    return render(request, 'catalog/products.html', context)
